2bfb62b1566e98eda7e07962e28600f1a2a8c918
Joseph Wilk <joe@josephwilk.net>
2017-11-24 13:55:08 +0000

    tidy

--- src/garbage/core.clj
+++ src/garbage/core.clj
@@ -2,24 +2,27 @@
   "Assumes timing is perfect. Which it never is..."
   (:require [overtone.osc :as osc]))
 
-(defonce midi-server (osc/osc-client "localhost" 4560))
+(def osc-midi-in-port 4562)
+(def erlang-port 4560)
+
+(defonce midi-client         (osc/osc-client "localhost" erlang-port))
 (def default-port    :iac_bus_1)
 (def default-channel 1)
 
-(defn time
+(defn ts
   ([]     (System/currentTimeMillis))
-  ([dur]  (+ (* 1000 dur) (time))))
+  ([dur]  (+ (* 1000 dur) (ts))))
 
 (defn midi-on-osc [channel note vel ts]
   (osc/osc-send-bundle
-   midi-server
+   midi-client
    (osc/osc-bundle
     ts
     (osc/osc-msg "/send_after" "localhost" (int 4561) "/iac_bus_1/note_on" (int channel) (int note) (int vel)))))
 
 (defn midi-off-osc [channel note ts]
   (osc/osc-send-bundle
-   midi-server
+   midi-client
    (osc/osc-bundle
     ts
     (osc/osc-msg "/send_after" "localhost" (int 4561) "/iac_bus_1/note_off" (int channel) (int note) (int 127)))))
@@ -27,12 +30,12 @@
 (defn midi-on  [n vel opts]
   (let [port    (get opts :port default-port)
         channel (get opts :channel default-channel)
-        ts (get opts :ts (time))]
-    (midi-osc channel n vel ts)))
+        ts (get opts :ts (ts))]
+    (midi-on-osc channel n vel ts)))
 
 (defn midi-off [n opts]
   (let [port :iac_bus_1
-        ts (get opts :ts (System/currentTimeMillis))]
+        ts (get opts :ts (ts))]
     (midi-off-osc (get opts :channel 1) n ts)))
 
 (defn midi
@@ -41,10 +44,10 @@
   ([n vel opts]
    (let [sustain (get opts :sustain 1)]
      (midi-on n vel opts)
-     (midi-off n (merge opts {:ts (time sustain)})))))
+     (midi-off n (merge opts {:ts (ts sustain)})))))
 
 (comment
-  (midi-on 54 127 {:channel 6})
+  (midi-on 54 127 {:channel 1})
   (midi-off 54 {:channel 6})
 
   (midi 54 127 {:channel 6 :sustain 2})
